4a2cb81c19fefb9837929ffc1dc25815e072b700
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-01 17:51:24 -0300

    Add files via upload

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+print("~ Convertidor de Grados Celsius (°C) a Fahrenheit (°F) ~")
+
+celsius = float(input("Ingresa los Grados Celsius a convertir: \n"))
+FORMULA_FAHRENHEIT = (celsius * 9/5) + 32
+
+print(celsius, "Grados Celsius son", FORMULA_FAHRENHEIT, "Grados Fahrenheit")
