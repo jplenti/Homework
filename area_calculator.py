bddf44b6e366cdb18989f4e56cced7f7dedba5a7
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-01 17:35:24 -0300

    Add files via upload

---
+++ area_calculator.py
@@ -0,0 +1,8 @@
+print("~ Calculadora de área de un triangulo ~")
+
+base = float(input("Ingresa la base: \n"))
+altura = float(input("Ingresa la altura: \n"))
+
+resultado = (base * altura) / 2
+
+print("El área del triangulo es:", resultado, "cm²")
