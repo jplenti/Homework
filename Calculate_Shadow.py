d813196f92356415364bc523469396fd24c1b20f
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-01 22:56:09 -0300

    Add files via upload

---
+++ Calculate_Shadow.py
@@ -0,0 +1,8 @@
+Height = 20 # cateto opuesto (h)
+# Angulo de elevacion = 22
+# Cateto Adyacente = longitud de la sombra
+Tangent = 0.4040 # Tangente(22)
+
+Shadow = Height / Tangent
+
+print("La longitud de la sombra de un edificio de", Height, "metros de altura es de", Shadow, "metros si los rayos del sol se encuentran en 22°")
