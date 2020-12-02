6150d3ca8a52ad2d0edab7c16a112ddec9caa605
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-01 23:06:34 -0300

    Add files via upload

---
+++ Two_ppl.py
@@ -0,0 +1,9 @@
+# TASK: Display True or False if the age entered by two users is the same.
+
+user1 = input("Introduce la edad del primer usuario: \n")
+user2 = input("Introduce la edad del segundo usuario: \n")
+
+if user1 == user2:
+    print("Is True")
+else:
+    print("Is False")
