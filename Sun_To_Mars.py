ee53b0026237fb799ba1a26067636b5ce37600b5
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-02 00:35:12 -0300

    Add files via upload

---
+++ Sun_To_Mars.py
@@ -0,0 +1,6 @@
+SUN_TO_MARS = 227940000 #distance from sun to mars
+LIGHT_SPEED = 300000 #km x secs
+
+Operation = SUN_TO_MARS/LIGHT_SPEED
+
+print("La luz tarda", Operation, "segundos viajar desde el Sol a Marte.")
