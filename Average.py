fcc7cf8a9158aa1e2cd37296975498457fcc824b
jplenti <75337174+jplenti@users.noreply.github.com>
2020-12-01 23:55:07 -0300

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,8 @@
+Spanish = float(input("Ingresa la nota final de Español: \n"))
+Maths = float(input("Ingresa la nota final de Matemáticas: \n"))
+Economy = float(input("Ingresa la nota final de Economía: \n"))
+Programming = float(input("Ingresa la nota final de Programación: \n"))
+English = float(input("Ingresa la nota final de Inglés: \n"))
+
+Average = (Spanish + Maths + Economy + Programming + English) / 5
+print("Tu promedio es:", Average)
